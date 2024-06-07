cf3f7f8a90f8046521bb9ab04bb221931a18c7f5
20155 <20155@LENOVO>
2024-06-07 16:09:12 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //String s = Console.ReadLine();
+
+
+
+            //char f = s[0];
+            //char l = s[s.Length -1];
+            //char tem = f;
+            //String news="";
+            //for (int i = 1; i < s.Length - 1; i++)
+            //{
+            //    news+= s[i];
+
+            //}
+            //Console.WriteLine(l+news+f);
+            //String se = "";
+
+
+            //if (s.Length < 3)
+            //{
+            //    for (int i = 0; i < 3; i++)
+            //    {
+            //        se += s;
+
+            //    }
+            //    Console.WriteLine(se);
+            //}
+            //else
+            //{
+
+
+            //        se += s[0];
+            //        se += s[1];
+            //        se += s[2];
+            //    String f = se;
+
+
+
+            //    se += s;
+            //    se += f;
+            //    Console.WriteLine(se);
+            //}
+
+            //char l = s[s.Length - 1];
+            //s = l + s + l;
+
+            //Console.Write(s);
+
+            //int n = int.Parse(Console.ReadLine());
+            //if (n % 3 == 0 || n % 7 == 0)
+            //{
+            //    Console.WriteLine("true");
+            //}
+            //else
+            //    Console.WriteLine("false");
+
+            /*
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+            if (Math.Min(n1,n2)<0 && Math.Max(n1,n2)>100)
+            {
+                Console.WriteLine("true");
+
+            }
+            else
+                Console.WriteLine("false");
+            */
+
+
+        }
+    }
+}
